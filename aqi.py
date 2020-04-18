a3795a8d93637d3cfff65137a4eb1613e7211a1e
pierrot10 <github@smart-idea.io>
2020-04-18 17:35:29 +0200

    Update aqi.py

--- aqi.py
+++ aqi.py
@@ -9,16 +9,46 @@ import adafruit_ssd1306, board, busio
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
+
 # 128x64 OLED Display
 display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
 # Clear the display.
 display.fill(0)
 display.show()
 width = display.width
 height = display.height
 
+# JSON
 JSON_FILE = '/var/www/html/aqi.json'
 
+# TinyLora
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
+
+# TinyLoRa Configuration
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = DigitalInOut(board.CE1)
+irq = DigitalInOut(board.D22)
+rst = DigitalInOut(board.D25)
+
+# TTN Device Address, 4 Bytes, MSB
+devaddr = bytearray([0x00, 0x00, 0x00, 0x00])
+# TTN Network Key, 16 Bytes, MSB
+nwkey = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+# TTN Application Key, 16 Bytess, MSB
+app = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+# Initialize ThingsNetwork configuration
+ttn_config = TTN(devaddr, nwkey, app, country='EU')
+lora = TinyLoRa(spi, cs, irq, rst, ttn_config)
+# 2b array to store sensor data
+data_pkt = bytearray(2)
+# time to delay periodic packet sends (in seconds)
+data_pkt_delay = 5.0
+
+#sds011
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
 
 def get_data(n=5):
@@ -102,15 +132,17 @@ while True:
         json.dump(data, outfile)
 
     # Sent to TTN
+    """
     try:
         #publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
         #save_log()
     except:
         print ("[INFO] Failure in sending data")
         #time.sleep(12)
+    """
 
     print('Sleep for 60sec')
     print(' ')
-    display.tex('Sleep for 60sec', 0, 50, 1)
+    display.text('Sleep for 60sec', 0, 50, 1)
     display.show()
     time.sleep(60)
